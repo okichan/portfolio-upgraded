fccfd2eab9b2a0f00486fb703b3e63531415d411
Tomomi <madeinwatashi@gmail.com>
2018-02-11 19:11:47 +1100

    Create main frames

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+   render() {
+      return (
+         <div className="App">
+            <div className="frame" id="card1">
+               <div className="frame-inner">
+                  text
+               </div>
+            </div>
+            
+            <div className="frame" id="card2">
+               <div className="frame-inner">
+                  text
+               </div>
+            </div>
+            
+            <div className="frame" id="card3">
+               <div className="frame-inner">
+                  text
+               </div>
+            </div>
+
+            <div className="frame" id="card4">
+               <div className="frame-inner">
+                  text
+               </div>
+            </div>
+
+            <div className="frame" id="card5">
+               <div className="frame-inner">
+                  text
+               </div>
+            </div>
+         </div>
+      );
+   }
+}
+
+export default App;
