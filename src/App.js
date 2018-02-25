a64dbe94df72cf74c6fc22ededa751ccdb3fb5d1
Tomomi <madeinwatashi@gmail.com>
2018-02-25 20:47:22 +1100

    Add 2nd card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import './assets/aos.css';
 import girl from './assets/coder_girl.png';
+import tsubaki from './assets/tsubaki.svg';
 import AOS from 'aos'
 
 class App extends Component {
@@ -27,10 +28,34 @@ class App extends Component {
                </div>
             </div>
 
-            <div className="frame" id="card2" data-aos="fade-right" data-aos-duration="1500">
+            {/* card 2 */}
+            <div className="frame" id="card2">
                <div className="title-frame">
-                  text
+                  <div className="title-frame-left">
+                     <img id="tsubaki" data-aos="flip-left" data-aos-duration="2000" data-aos-offset="100" alt="tsubaki" src={tsubaki} />
+                  </div>
+                  <div className="title-frame-right" >
+                     <h3 className="">
+                        About Me
+                     </h3>
+                  </div>
                </div>
+               <p>Why, hello there.</p>
+               <p>My name is Tomomi, born and bred Japanese living in Melbourne.
+               </p>
+               <p>
+                  This portfolio was originally created to showcase my skills that I've learnt in the first 1.5 month of studying at <a href="https://coderacademy.edu.au" target="_blank">Coder Academy</a>.
+                  It is maintained and updated occasionally ever since.
+               </p>
+               <ul>
+                  <li>
+                     Past profession: A survey programmer in marketing research industry
+                  </li>
+                  <li>
+                     <i className="fa fa-heart"></i>
+                     : Music, food, art and code (especially front-end!)
+                  </li>
+               </ul>
             </div>
 
             <div className="frame" id="card3">
@@ -50,6 +75,7 @@ class App extends Component {
                   text
                </div>
             </div>
+            Last updated on: Feb 2018
          </div>
       );
    }
